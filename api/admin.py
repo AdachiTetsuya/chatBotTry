7f00349f8cd8d041f13387c275b538c533386d8c
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-01 20:48:45 +0900

    fix

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["id", "username", "line_id"]
+    list_filter = []
+    ordering = []
+    filter_horizontal = []
+    fieldsets = (
+        (
+            None,
+            {"fields": ()},
+        ),
+        (
+            ("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+    )
