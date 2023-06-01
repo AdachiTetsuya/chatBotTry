38656f063a52b4f884f5b882c10d4fb586a606b3
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-01 16:25:18 +0900

    logger追加

--- api/views.py
+++ api/views.py
@@ -1,26 +1,33 @@
-from .bot_base import LineBotMSG
-from .bot_messages import create_text_message
+import logging
+
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+
+from .bot_base import LineBotMSG
+from .bot_messages import create_text_message
+
+logger = logging.getLogger("api")
 
 
 line_message = LineBotMSG()
+
 
 class LineBotApiView(APIView):
     @method_decorator(csrf_exempt)
     def post(self, request, format=None):
         res = request.data
-        response_text = {"text": "正常に検索されました。"} #レスポンスとして返す
-        code = status.HTTP_200_OK #レスポンスのステータスコード
-        if len(res['events']) > 0:
-            data = res['events'][0] #リストの中に辞書がひとつ
-            text = data['message'].get('text')
+        response_text = {"text": "正常に検索されました。"}  # レスポンスとして返す
+        code = status.HTTP_200_OK  # レスポンスのステータスコード
+        if len(res["events"]) > 0:
+            data = res["events"][0]  # リストの中に辞書がひとつ
+            if data["message"]:
+                logger.info(res["events"])
+                logger.info(data["message"]["text"])
 
-            line_message.reply(data['replyToken'], create_text_message("テスト用ボットのテキスト"))
+            line_message.reply(data["replyToken"], create_text_message("テスト用ボットのテキスト"))
         else:
-            response_text["text"] = "Webhookの確認" #確認時に返すレスポンス
+            response_text["text"] = "Webhookの確認"  # 確認時に返すレスポンス
         return Response(response_text, status=code)
--- be/settings.py
+++ be/settings.py
@@ -123,3 +123,52 @@ STATIC_URL = "/static/"
 ALLOWED_HOSTS = [s.strip() for s in os.getenv("ALLOWED_HOSTS", "").split(",") if s]
 CORS_ALLOWED_ORIGINS = [s.strip() for s in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if s]
 CORS_ALLOW_CREDENTIALS = True
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "filters": ["require_debug_true"],
+            "level": "INFO",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "level": "INFO",
+            "filename": "./debug.log",
+        },
+        "mail_admins": {
+            "class": "django.utils.log.AdminEmailHandler",
+            "filters": ["require_debug_false"],
+            "include_html": True,
+            "level": "ERROR",
+        },
+        "null": {
+            "class": "logging.NullHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "INFO",
+        },
+        "django.security.DisallowedHost": {
+            "handlers": ["null"],
+            "propagate": False,
+        },
+        "api": {
+            "handlers": ["file"],
+            "level": "INFO",
+        },
+    },
+}
