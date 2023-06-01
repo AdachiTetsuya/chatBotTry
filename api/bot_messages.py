b8edcb8b1bd16e8457039e4decafe6ac3b70f583
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-01 17:31:45 +0900

    fix

--- api/bot_messages.py
+++ api/bot_messages.py
@@ -1,8 +1,4 @@
-def create_text_message(msg):
-    message = [
-                {
-                    'type': 'text',
-                    'text': msg
-                }
-            ]
+def create_text_message_list(*args):
+    if args:
+        message = [{"type": "text", "text": msg} for msg in args]
     return message
--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .bot_base import LineBotMSG
-from .bot_messages import create_text_message
+from .bot_messages import create_text_message_list
 
 logger = logging.getLogger("api")
 
@@ -19,14 +19,11 @@ class LineBotApiView(APIView):
     @method_decorator(csrf_exempt)
     def post(self, request, format=None):
         res = request.data
-        response_text = {"text": "正常に検索されました。"}  # レスポンスとして返す
-        code = status.HTTP_200_OK  # レスポンスのステータスコード
-        if len(res["events"]) > 0:
+
+        if res["events"]:
             data = res["events"][0]  # リストの中に辞書がひとつ
             if data["message"]:
-                logger.info(data["replyToken"])
-
-                line_message.reply(data["replyToken"], create_text_message(data["message"]["text"]))
+                line_message.reply(data["replyToken"], create_text_message_list("こんにちわ", "こんばんわ"))
         else:
-            response_text["text"] = "Webhookの確認"  # 確認時に返すレスポンス
-        return Response(response_text, status=code)
+            response_text = {"text": "正常に検索されました。"}  # レスポンスとして返す
+        return Response(response_text, status=status.HTTP_200_OK)
