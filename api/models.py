f56863d9458d0597fe5c8a74b9fffd0398234f6d
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-02 14:33:38 +0900

    poll と バディ情報のモデル追加

--- api/models.py
+++ api/models.py
@@ -12,6 +12,21 @@ class CustomUser(models.Model):
         verbose_name_plural = "Line アカウント"
 
 
+class SmartPoll(models.Model):
+    default_name = models.CharField("ポール名", max_length=30)
+
+    class Meta:
+        verbose_name_plural = "スマートポール"
+
+
+class buddyInformation(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="buddy_information")
+    smart_poll = models.ForeignKey(SmartPoll, on_delete=models.CASCADE, related_name="user")
+
+    class Meta:
+        verbose_name_plural = "バディー情報"
+
+
 class UnknownMessage(models.Model):
     text = models.TextField()
 
