ae78edac81199af2e0256301d712889a56a781a3
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-03 02:07:42 +0900

    fix

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class CustomUser(models.Model):
     username = models.CharField("ユーザー名", max_length=50, blank=True, null=True)
-    line_id = models.CharField("LINE ID", max_length=30, unique=True)
+    line_id = models.CharField("LINE ID", max_length=50, unique=True)
 
     def __str__(self):
         return self.username
