94a4085924219d0b950ab5299feebee9b43559bd
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-06-18 18:18:58 +0900

    中断処理を追加

--- api/sequences/judge_sequence.py
+++ api/sequences/judge_sequence.py
@@ -1,40 +1,44 @@
 from api.about_comment_count import update_comment_count_and_relationship_level
 from api.data.operation import OPERATION_DATA, PERSONAL_OPERATION_DATA
 from api.mecab_function import wakati_text
+from api.models import UserPollRelation
 from api.utils import get_message_text
+
+
+def judge_poll_name_contain(message, user_poll_relations: list[UserPollRelation]):
+    poll_name_list = [item.poll_name for item in user_poll_relations]
+    for i, poll_name in enumerate(poll_name_list):
+        if poll_name in message:
+            return user_poll_relations[i]
 
 
 def judge_sequence_from_message(event_obj, user_poll_relations, user):
     message = get_message_text(event_obj)
-    separated_text_list = wakati_text(message)
+    target_poll = judge_poll_name_contain(message, user_poll_relations)
 
-    poll_name_list = [item.poll_name for item in user_poll_relations]
+    separated_text_list = wakati_text(message)
 
     result = {"operation": "", "target": ""}
 
     # ポールの名前が入ってる場合の処理
-    for i, poll_name in enumerate(poll_name_list):
-        if poll_name not in separated_text_list:
-            continue
-
-        user_poll_relation = user_poll_relations[i]
-        update_comment_count_and_relationship_level(user_poll_relation)
+    if target_poll:
+        update_comment_count_and_relationship_level(target_poll)
 
         for op_name, op_word_list in PERSONAL_OPERATION_DATA.items():
             if type(op_word_list[0]) is str:
                 for op_word in op_word_list:
                     if op_word in separated_text_list:
                         result["operation"] = op_name
-                        result["target"] = user_poll_relation
+                        result["target"] = target_poll
                         return result
             else:
                 if set(op_word_list[0]).issubset(separated_text_list):
                     result["operation"] = op_name
-                    result["target"] = user_poll_relation
+                    result["target"] = target_poll
                     return result
 
         result["operation"] = "single_response"
-        result["target"] = user_poll_relations[i]
+        result["target"] = target_poll
 
     # 「私」という言葉が入ってる場合の処理
     if "私" in separated_text_list:
