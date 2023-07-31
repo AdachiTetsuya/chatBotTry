468ed43a7efb4132fbe4901c5bad1c7da100ba30
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-07-31 23:15:22 +0900

    プッシュメッセージ送ってみる

--- api/tasks.py
+++ api/tasks.py
@@ -1,6 +1,14 @@
 from celery import shared_task
 
+from api.bot_base import LineBotMSG
+from api.bot_messages import create_text_message_list
+
+line_message = LineBotMSG()
+
 
 @shared_task
 def say_hello():
-    print("Hello")
+    result = create_text_message_list("こんにちわ")
+    id = "U9832203d5f9ab9edecff7fb10b003d77"
+
+    line_message.push_message(id, result)
