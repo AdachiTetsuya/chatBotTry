f869d4a2562837a5cdf6a1ed81c5f0f8f6add567
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-08-01 19:41:43 +0900

    GINZA

---
+++ languages.py
@@ -0,0 +1,5 @@
+# GINZAモデル
+class GINZA:
+    ISO_639_1 = "ja_ginza"
+    ISO_639 = "ginza"
+    ENGLISH_NAME = "ginza"
---
+++ train_script.py
@@ -0,0 +1,11 @@
+from chatterbot import ChatBot
+
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+import languages  # 自作言語モジュール
+
+chatbot = ChatBot(
+    "test",
+    tagger_language=languages.GINZA,
+    database_uri="sqlite:///database.db",
+)
+response = chatbot.get_response("調子はどう？")
